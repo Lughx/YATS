62edd694a6fc222f7a76faeed27db7bdde738be4
yahirPC <lughxdr@gmail.com>
2024-02-29 20:51:58 -0600

    navbar right fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export function Navbar() {
     return (
         <nav className="mb-3 backdrop-blur-[2px] fixed z-10 w-full">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-center md:justify-end mx-auto p-4">
-                <div className="w-full block w-auto">
+            <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+                <div className="w-full block w-auto flex justify-center md:justify-end">
                     <ul className="font-medium flex p-0 mt-4 rounded-lg flex-row space-x-8 rtl:space-x-reverse mt-0 border-0">
                         <li>
                             <a href="#home" className="block text-white rounded hover:text-blue-700 p-0">Inicio</a>
